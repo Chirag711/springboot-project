40d2b76960481a7ef9526ab7ae61715d3b353415
Chirag711 <chiragdraul@gmail.com>
2026-02-28 12:26:56 +0530

    UserRepository

--- first-project/src/main/java/com/example/firstproject/repository/UserRepository.java
+++ first-project/src/main/java/com/example/firstproject/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.firstproject.model.User;
 
 public interface UserRepository extends MongoRepository<User,String> {
-
+	User findByEmail(String email);
+	
+	User findByResetToken(String token);
 }
