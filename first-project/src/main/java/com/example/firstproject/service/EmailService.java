28fdc06cc0c8480a9dfe6116d14b8c75aa43961c
Chirag711 <chiragdraul@gmail.com>
2026-02-28 13:05:01 +0530

    app and email service update

--- first-project/src/main/java/com/example/firstproject/service/EmailService.java
+++ first-project/src/main/java/com/example/firstproject/service/EmailService.java
@@ -1,5 +1,21 @@
 package com.example.firstproject.service;
 
-public class EmailService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+public class EmailService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void sendEmail(String to, String subject, String text) {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setText(to);
+		msg.setSubject(subject);
+		msg.setText(text);
+		mailSender.send(msg);
+	}
 }
