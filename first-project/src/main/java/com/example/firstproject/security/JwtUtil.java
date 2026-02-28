ee0d8646143c7f04504324d177a3847bbc7088e4
Chirag711 <chiragdraul@gmail.com>
2026-02-28 12:51:58 +0530

    jwt-util update

--- first-project/src/main/java/com/example/firstproject/security/JwtUtil.java
+++ first-project/src/main/java/com/example/firstproject/security/JwtUtil.java
@@ -1,5 +1,31 @@
 package com.example.firstproject.security;
 
-public class JwtUtil {
+import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.*;
+
+
+@Component
+public class JwtUtil {
+	private String SECRET = "thisisaspringapplication";
+	
+	public String generateToken(String email) {
+		return Jwts.builder()
+				.setSubject(email)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis()+10800000))
+				.signWith(SignatureAlgorithm.HS256, SECRET)
+				.compact();
+	}
+	
+	public String extractEmail(String token) {
+		return Jwts.parser()
+				.setSigningKey(SECRET)
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
+	
 }
